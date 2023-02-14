aba8bcad744f409de72d6c33947a701ac054f0ef
AnhTran09 <anh.tran.edu@gmail.com>
2023-02-14 09:33:26 -0600

    changing it to classes

--- csce3513_project/Splash.py
+++ csce3513_project/Splash.py
@@ -1,47 +1,53 @@
 from tkinter import *
-from Page import Page
+from csce3513_project.Page import Page
 from PIL import Image, ImageTk
 
 
-
-# set up blank screen
-splash_root = Tk()
-
-width = 427
-height = 250
-screen_width = splash_root.winfo_screenwidth()
-screen_height = splash_root.winfo_screenheight()
-x_coordinate = (screen_width / 2) - (width / 2)
-y_coordinate = (screen_height / 2) - (height / 2)
-
-# adjust where it pops up
-splash_root.geometry("%dx%d+%d+%d" % (width, height, x_coordinate, y_coordinate))
-
-# remove the heading on top
-splash_root.overrideredirect(1)
-
-
-
-a = '#000000'
-Frame(splash_root, width=427, height=241, bg='black').place(x=0, y=0)
-
-
-# add logo
-im = Image.open(r"C:\Users\14794\Documents\Software_Engineering\GUI\logo.jpg")
-logo = im.resize((width, height))
-LOGO = ImageTk.PhotoImage(logo)
-
-# insert logo
-logo_label = Label(image=LOGO)
-logo_label.place(x = 0, y = 0)
-
-
 def run_page():
-    splash_root.destroy()
-    Page()
+    return Page()
+
+class Splash:
+
+    def __init__(self) -> None:
+        # set up blank screen
+        width = 427
+        height = 250
+        self.root = Tk()
+
+
+        screen_width = self.root.winfo_screenwidth()
+        screen_height = self.root.winfo_screenheight()
+        x_coordinate = (screen_width / 2) - (width / 2)
+        y_coordinate = (screen_height / 2) - (height / 2)
+
+        # adjust where it pops up
+        self.root.geometry("%dx%d+%d+%d" % (width, height, x_coordinate, y_coordinate))
+
+        # remove the heading on top
+        self.root.overrideredirect(1)
+
+        Frame(self.root, width=427, height=241, bg='black').place(x=0, y=0)
+
+
+        # add logo
+        im = Image.open("csce3513_project\logo.jpg")
+        logo = im.resize((width, height))
+        LOGO = ImageTk.PhotoImage(logo)
+
+        # insert logo
+        logo_label = Label(image=LOGO, bg = 'black')
+        logo_label.place(x = 0, y = 0)
+
+        self.root.after(3000,lambda: self.root.destroy())
+        mainloop()
+        run_page()
 
 
 
-splash_root.after(3000,run_page)
 
-mainloop()
+
+
+
+
+
+
