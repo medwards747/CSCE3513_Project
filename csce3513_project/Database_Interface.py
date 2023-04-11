5ce165ca0ac459bb4646c724787b00fc79c8047f
Matthew Edwards <me004@uark.edu>
2023-04-11 11:32:45 -0500

    Updating Connection DB

--- csce3513_project/Database_Interface.py
+++ csce3513_project/Database_Interface.py
@@ -2,14 +2,13 @@
 from supabase import create_client, Client
 
 
-
 class Database_Interface:
-    url = "https://wceblbcdgfoqocminldo.supabase.co"
-    key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndjZWJsYmNkZ2ZvcW9jbWlubGRvIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzYzMTg5MDUsImV4cCI6MTk5MTg5NDkwNX0.b15JOIrzQJM_hyHJN1-TdDZQb25zRuRp32ybMW4l5hw"
+    url = "https://gmqxbfvbtseyxorhjfct.supabase.co"
+    key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdtcXhiZnZidHNleXhvcmhqZmN0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODEyMzA1MDYsImV4cCI6MTk5NjgwNjUwNn0.cH9g_PM6S845tTKfvUZrlu6UeEYB9lMYnPBXAlQOSaY"
     supabase: Client = create_client(url, key)
 
     def searchID(self, id):
-        result = self.supabase.table("player").select("*").eq("id",id).execute().data
+        result = self.supabase.table("player").select("*").eq("id",id).explain().execute().data
         if result == []:
             return(False)
         else:
